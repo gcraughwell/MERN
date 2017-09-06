1bad7bd9606773e03ba40ae52f4db700202377e0
gcraughwell <garycraughwell@Garys-Mac-Pro.local>
2017-09-06 14:04:55 -0400

    added billing

--- index.js
+++ index.js
@@ -29,17 +29,17 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-//express will serve up production assets
-//like our main.js file, or main.css file 
+  //express will serve up production assets
+  //like our main.js file, or main.css file
   app.use(express.static('client/build'));
 
   //express will serve up index.html file
   //if it does not recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // if process.evn.PORT if a point is not already provided use 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
